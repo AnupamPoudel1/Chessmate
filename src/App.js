f40f34feb7805201a1a2ab002c9c7a363119569f
Anupam <anupampoudel78@gmail.com>
2024-01-21 20:30:35 +0545

    chessboard and chess pieces in order

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 // import Login from "./pages/Login";
 // import Signup from "./pages/Signup";
-import Board from "./components/Chessboard";
+import Board from "./components/chessboard/Board";
 
 function App() {
   return (
-    <div className="flex justify-center items-center h-screen w-full overflow-auto">
+    <div className="flex justify-center items-center h-screen w-full bg-black text-white overflow-hidden">
       <Board />
     </div>
   );
